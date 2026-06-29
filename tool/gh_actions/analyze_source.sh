3519738fa93d7ec3da002ee422858e0dcdbae0d9
Desmond A. Kirkpatrick <desmond.a.kirkpatrick@intel.com>
2026-06-28 17:52:35 -0700

    fix analyze_source.sh to run pub get, then analyze

--- tool/gh_actions/analyze_source.sh
+++ tool/gh_actions/analyze_source.sh
@@ -15,7 +15,7 @@ dart analyze --fatal-infos
 
 # Analyze sub-packages that have their own pubspec.yaml and are excluded
 # from the root analysis_options.yaml.
-for pkg in packages/rohd_hierarchy; do
+for pkg in packages/rohd_hierarchy packages/rohd_waveform; do
   if [ -f "$pkg/pubspec.yaml" ]; then
     echo "Analyzing sub-package: $pkg"
     pushd "$pkg" > /dev/null
